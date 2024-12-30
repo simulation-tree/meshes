275330acec2651acb186309e6b38b9eb4dc3651a
Phill <philldamaskin@gmail.com>
2024-12-30 13:32:10 -0500

    simplify type layout registration

--- tests/MeshTests.cs
+++ tests/MeshTests.cs
@@ -13,14 +13,14 @@ namespace Meshes.Tests
 
         static MeshTests()
         {
-            TypeLayout.Register<IsMesh>("IsMesh");
-            TypeLayout.Register<MeshVertexPosition>("MeshVertexPosition");
-            TypeLayout.Register<MeshVertexNormal>("MeshVertexNormal");
-            TypeLayout.Register<MeshVertexUV>("MeshVertexUV");
-            TypeLayout.Register<MeshVertexColor>("MeshVertexColor");
-            TypeLayout.Register<MeshVertexTangent>("MeshVertexTangent");
-            TypeLayout.Register<MeshVertexBiTangent>("MeshVertexBiTangent");
-            TypeLayout.Register<MeshVertexIndex>("MeshVertexIndex");
+            TypeLayout.Register<IsMesh>();
+            TypeLayout.Register<MeshVertexPosition>();
+            TypeLayout.Register<MeshVertexNormal>();
+            TypeLayout.Register<MeshVertexUV>();
+            TypeLayout.Register<MeshVertexColor>();
+            TypeLayout.Register<MeshVertexTangent>();
+            TypeLayout.Register<MeshVertexBiTangent>();
+            TypeLayout.Register<MeshVertexIndex>();
         }
 
         protected override void SetUp()
